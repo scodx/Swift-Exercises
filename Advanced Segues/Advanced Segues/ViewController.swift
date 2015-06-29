ec11f0dde937411e9430aa3ad38407215692144d
Oscar Sánchez <oscar.exe@gmail.com>
2015-06-28 23:59:48 -0400

    advanced seques.

---
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Advanced Segues
+//
+//  Created by Oscar on 28/06/15.
+//  Copyright (c) 2015 uinik. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println(rowCounter)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
