eb80bb70acc444c6e78caa016f8c16d3dfda20f3
Oscar Sánchez <oscar.exe@gmail.com>
2015-07-01 19:00:00 -0400

    memorable places excercise, not finished :)

---
+++ Memorable Places/Memorable Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by Oscar on 29/06/15.
+//  Copyright (c) 2015 uinik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
